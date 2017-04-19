0d6f69c9482c5555a646f199884c1878529fd115
bensmith <benhsmith13@gmail.com>
2017-04-19 08:29:30 -0600

    more ships

--- scripts/state/ships/ship_directory.js
+++ scripts/state/ships/ship_directory.js
@@ -6,21 +6,37 @@ class ShipDirectory {
   }
 
   getRandomShip() {
-    const rand = _.random(100);
+    const rand = _.random(110);
   //  Modify this logic to adjust how frequently different ships appear
-    if (rand < 33) {
+    if (rand < 10) {
       return this.ships.lightRunner;
-    } else if (rand < 66) {
+    } else if (rand < 20) {
       return this.ships.scoutShip;
-    } else {
+    } else if (rand < 30) {
+      return this.ships.battleShip;
+    } else if (rand < 40) {
+      return this.ships.heavyBattleShip;
+    } else if (rand < 50) {
+      return this.ships.heavyGunShip;
+    } else if (rand < 60) {
+      return this.ships.heavySpeeder;
+    } else if (rand < 70) {
+      return this.ships.brigadier;
+    } else if (rand < 80) {
+      return this.ships.speeder;
+    } else if (rand < 90) {
       return this.ships.gunShip;
+    } else if (rand < 100) {
+      return this.ships.necroCruiser;
+    } else if (rand < 110) {
+      return this.ships.crusader;
     }
   };
 
   directory() {
     return {
       lightRunner: {
-        type: 'lightRunner',
+        type: 'lightRunner',  // 1
         height: 95 * shipScale,
         width: 227 * shipScale,
         hp: 20,
@@ -30,7 +46,7 @@ class ShipDirectory {
         reloadTime: 1,
       },
       scoutShip: {
-        type: 'scoutShip',
+        type: 'scoutShip',  // 2
         height: 69 * shipScale,
         width: 105 * shipScale,
         hp: 20,
@@ -39,8 +55,78 @@ class ShipDirectory {
         bulletDirection: () => _.random(30, 160),
         reloadTime: 0.5,
       },
+      battleShip: {
+        type: 'battleShip',  // 3
+        height: 124 * shipScale,
+        width: 290 * shipScale,
+        hp: 20,
+        speed: 20,
+        bulletType: 'normal',
+        bulletDirection: () => _.random(30, 160),
+        reloadTime: 0.5,
+      },
+      heavyBattleShip: {
+        type: 'heavyBattleShip',  // 4
+        height: 124 * shipScale,
+        width: 290 * shipScale,
+        hp: 20,
+        speed: 20,
+        bulletType: 'normal',
+        bulletDirection: () => _.random(30, 160),
+        reloadTime: 0.5,
+      },
+      speeder: {
+        type: 'speeder', // 9.png
+        height: 45 * shipScale,
+        width: 127 * shipScale,
+        hp: 30,
+        speed: 30,
+        bulletType: 'normal',
+        bulletDirection: () => _.random(30, 160),
+        reloadTime: 0.5,
+      },
+      heavySpeeder: {
+        type: 'heavySpeeder', // 6.png
+        height: 100 * shipScale,
+        width: 234 * shipScale,
+        hp: 30,
+        speed: 30,
+        bulletType: 'normal',
+        bulletDirection: () => _.random(30, 160),
+        reloadTime: 0.5,
+      },
+      brigadier: {
+        type: 'brigadier',  // 7
+        height: 136 * shipScale,
+        width: 288 * shipScale,
+        hp: 20,
+        speed: 20,
+        bulletType: 'normal',
+        bulletDirection: () => _.random(30, 160),
+        reloadTime: 0.5,
+      },
+      crusader: {
+        type: 'crusader',  // 13
+        height: 113 * shipScale,
+        width: 223 * shipScale,
+        hp: 60,
+        speed: 15,
+        bulletType: 'normal',  // I need more bullet types
+        bulletDirection: 135,
+        reloadTime: 1.5,
+      },
+      necroCruiser: {
+        type: 'necroCruiser',  // 12
+        height: 135 * shipScale,
+        width: 289 * shipScale,
+        hp: 60,
+        speed: 15,
+        bulletType: 'normal',  // I need more bullet types
+        bulletDirection: 135,
+        reloadTime: 1.5,
+      },
       gunShip: {
-        type: 'gunShip',
+        type: 'gunShip', // 10
         height: 157 * shipScale,
         width: 213 * shipScale,
         hp: 40,
@@ -48,7 +134,17 @@ class ShipDirectory {
         bulletType: 'heavy',
         bulletDirection: 90,
         reloadTime: 1.5,
-      }
+      },
+      heavyGunShip: {
+        type: 'heavyGunShip', // 5.png
+        height: 390 * shipScale,
+        width: 523 * shipScale,
+        hp: 100,
+        speed: 3,
+        bulletType: ['heavy', 'normal'], // TODO: multiple types?
+        bulletDirection: 90,
+        reloadTime: 1.5,
+      },
     }
   }
 }
--- scripts/ui/sprites/ship_assets.js
+++ scripts/ui/sprites/ship_assets.js
@@ -18,7 +18,15 @@ class ShipAssets {
    return {
      lightRunner: this.makeImage('./assets/ships/1.png'),
      scoutShip: this.makeImage('./assets/ships/2.png'),
+     battleShip: this.makeImage('./assets/ships/3.png'),
+     heavyBattleShip: this.makeImage('./assets/ships/4.png'),
+     heavyGunShip: this.makeImage('./assets/ships/5.png'),
+     heavySpeeder: this.makeImage('./assets/ships/6.png'),
+     brigadier: this.makeImage('./assets/ships/7.png'),
+     speeder: this.makeImage('./assets/ships/9.png'),
      gunShip: this.makeImage('./assets/ships/10.png'),
+     necroCruiser: this.makeImage('./assets/ships/12.png'),
+     crusader: this.makeImage('./assets/ships/13.png'),
    }
   }
 }
